600ab137a9adeba2069ddded1999cf300c9889f9
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-12-08 15:57:08 -0800

    pipeline logic (3)

--- openshift/clean-openshift-project.sh
+++ openshift/clean-openshift-project.sh
@@ -2,14 +2,13 @@
 
 [ -f ~/playground/openshift/oc-bash/functions ] && . ~/playground/openshift/oc-bash/functions || { echo "~/playground/openshift/oc-bash/functions not found" 1>&2; exit 1; }
 
-openshift_project_name="novaordis-dev"
+openshift_project_name="noss-dev-pipeline"
 
 function main() {
 
     verify-project-name ${openshift_project_name}
 
-    oc-delete bc/novaordis-session-servlet
-    oc-delete is/novaordis-session-servlet
+    oc-delete bc/noss
 
 
 }
